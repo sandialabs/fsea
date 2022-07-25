9bf07b88e4d460d59ab79c09a5717f4cbff778a8
Sam Eaton <108030273+sweaton9@users.noreply.github.com>
2022-07-25 13:51:11 -0500

    Add files via upload
    Updated parameters.

---
+++ config.py
@@ -0,0 +1,27 @@
+
+path = '' # path to folder containing project files
+filename = 'WACO_Station_Acoustic_Data.parquet'
+filepath = path + filename
+
+gt_filename = 'Tachometer_Ground_Truth.parquet'
+gt_filepath = path + gt_filename
+
+samples_per_second = 100
+window = 90
+overlap = 0.5
+
+start = '2018-09-16T14:00:00'
+stop = '2018-09-16T18:00:00'
+
+freq_lims = [10, 42]
+spl_lims = [40, 70]
+
+gear_ratio = 11.14
+blades = 8
+span = 3
+fan_number_guess = 3
+total_fans = 4
+max_fan_bpf = 21.54
+total_filter_height = 5
+mid_filter_height = 3
+harmonics = [1, 2]
